ab41c7c73a1ead74de5eda2dc54954debfde2b28
isidro <alconcci22@gmail.com>
2023-12-11 22:45:53 -0600

    Consutltorio Web

---
+++ js/sitio.js
@@ -0,0 +1,14 @@
+new WOW().init();
+
+//
+// lightbox.option({
+//       'resizeDuration': 500,
+//       'wrapAround': true,
+//     alwaysShowNavOnTouchDevices: true,
+//     showImageNumberLabel: false,
+//     positionFromTop: 75,
+//     imageFadeDuration: 200,
+//     fadeDuration: 1000,
+//     disableScrolling: true
+//
+//     })
